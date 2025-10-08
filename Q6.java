cc04783c964ab961bc5475ed575d02a324e06444
NicolasCHANTEUX <166408814+NicolasCHANTEUX@users.noreply.github.com>
2025-10-08 16:12:05 +0200

    Add Q6.java to print decreasing rows of 'x'

---
+++ Q6.java
@@ -0,0 +1,11 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("x ");
+            }
+            System.out.println();
+        }
+    }
+}
